f44e3185edccc73db3fd60adf0fea2f81acd21e1
narender-t667 <narendergoud2004@gmail.com>
2025-07-23 11:55:47 +0530

    Create job_alert.py

---
+++ job_alert.py
@@ -0,0 +1,25 @@
+import smtplib
+from datetime import date
+from email.mime.text import MIMEText
+
+JOBS = [
+    "https://in.linkedin.com/jobs/entry-level-data-science-jobs",
+    "https://www.indeed.com/q-entry-level-python-developer-jobs.html",
+    "https://angel.co/jobs",
+    "https://internshala.com/internships/machine-learning%2C-python-jobs",
+]
+
+def send_email():
+    body = "📢 Job Links for Today:\n\n" + "\n".join(JOBS)
+    msg = MIMEText(body)
+    msg["Subject"] = f"{date.today()} – Entry-Level Tech Job Links"
+    msg["From"] = "pavanrayanchu03@gmail.com@gmail.com"
+    msg["To"] = "rayanchupavanrayanchu@gmail.com"
+
+    s = smtplib.SMTP_SSL("smtp.gmail.com", 465)
+    s.login("pavanrayanchu03@gmail.com", "Rpavan@03")
+    s.send_message(msg)
+    s.quit()
+
+if __name__ == "__main__":
+    send_email()
