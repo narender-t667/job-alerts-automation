6a5fb6592c3e184423b5078f71f0123986037223
narender-t667 <narendergoud2004@gmail.com>
2025-07-23 12:09:09 +0530

    Update job_alert.py

--- job_alert.py
+++ job_alert.py
@@ -13,13 +13,14 @@ def send_email():
     body = "📢 Job Links for Today:\n\n" + "\n".join(JOBS)
     msg = MIMEText(body)
     msg["Subject"] = f"{date.today()} – Entry-Level Tech Job Links"
-    msg["From"] = "pavanrayanchu03@gmail.com@gmail.com"
-    msg["To"] = "rayanchupavanrayanchu@gmail.com"
+    msg["From"] = "you@gmail.com"
+    msg["To"] = "you@gmail.com"
 
     s = smtplib.SMTP_SSL("smtp.gmail.com", 465)
-    s.login("pavanrayanchu03@gmail.com", "Rpavan@03")
+    s.login("you@gmail.com", "YOUR_APP_PASSWORD")
     s.send_message(msg)
     s.quit()
 
 if __name__ == "__main__":
     send_email()
+
